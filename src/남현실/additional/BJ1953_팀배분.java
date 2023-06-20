a9954689451c18345949687da57cb0c0bb75f27d
hsilnam <hsilnam3@gmail.com>
2023-06-20 17:41:53 +0900

    fix: 1953 code err

--- src/남현실/additional/BJ1953_팀배분.java
+++ src/남현실/additional/BJ1953_팀배분.java
@@ -1,5 +1,9 @@
 package 남현실.additional;
 
+/*
+https://www.acmicpc.net/problem/1953
+ */
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -17,66 +21,64 @@ public class BJ1953_팀배분 {
         for (int i = 0; i < N; i++) {
             list[i] = new ArrayList<>();
         }
-        boolean[] visited = new boolean[N];
 
         for (int i = 0; i < N; i++) {
             temp = br.readLine().split(" ");
             for (int j = 1; j <= Integer.parseInt(temp[0]); j++) {
-                list[i].add(Integer.parseInt(temp[j])-1);
+                list[i].add(Integer.parseInt(temp[j]) - 1);
             }
         }
 
-        ArrayList<Integer> groupA = new ArrayList<>();
-        ArrayList<Integer> groupB = new ArrayList<>();
+        ArrayList<Integer>[] group = new ArrayList[2];
+        for (int i = 0; i < 2; i++) {
+            group[i] = new ArrayList<>();
+        }
 
+        boolean[] visited = new boolean[N];
+        int[] groupNum = new int[N];
         for (int i = 0; i < N; i++) {
-            if(!visited[i]) {
+            if (!visited[i]) {
                 Queue<Integer> queue = new ArrayDeque<>();
                 queue.offer(i);
                 visited[i] = true;
-                groupA.add(i);
+                group[0].add(i);
+                groupNum[i] = 0;
 
 
                 int cur;
-                int k = 1;
                 while (!queue.isEmpty()) {
                     cur = queue.poll();
 
-                    for (int next: list[cur]) {
-                        if(visited[next]) {
+                    int gnum = (groupNum[cur] + 1) % 2;
+                    for (int next : list[cur]) {
+                        if (visited[next]) {
                             continue;
                         }
                         queue.offer(next);
                         visited[next] = true;
-                        if(k == 0) {
-                            groupA.add(next);
-                            k = 1;
-                        } else {
-                            groupB.add(next);
-                            k = 0;
-                        }
+                        group[gnum].add(next);
+                        groupNum[next] = gnum;
                     }
                 }
             }
         }
 
-        if(groupB.isEmpty()) {
-            groupA.remove(0);
-            groupB.add(0);
+        if (group[1].isEmpty()) {
+            group[0].remove(1);
+            group[1].add(1);
         }
-
-        Collections.sort(groupA);
-        Collections.sort(groupB);
+        Collections.sort(group[0]);
+        Collections.sort(group[1]);
 
         StringBuilder result = new StringBuilder();
-        result.append(groupA.size()).append("\n");
-        for (int num: groupA) {
-            result.append(num+1).append(" ");
+        result.append(group[0].size()).append("\n");
+        for (int num : group[0]) {
+            result.append(num + 1).append(" ");
         }
         result.append("\n");
-        result.append(groupB.size()).append("\n");
-        for (int num: groupB) {
-            result.append(num+1).append(" ");
+        result.append(group[1].size()).append("\n");
+        for (int num : group[1]) {
+            result.append(num + 1).append(" ");
         }
         result.append("\n");
 
