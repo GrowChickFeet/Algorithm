3921db1cb034d775fc99674917f3e9bdc55c3cdc
hsilnam <hsilnam3@gmail.com>
2023-06-25 23:57:43 +0900

    add: 11726 code

---
+++ src/남현실/week8/BJ11726_2xn_타일링.java
@@ -0,0 +1,23 @@
+package 남현실.week8;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+/*
+2*N을 1*2, 2*1타일로 채우기
+ */
+public class BJ11726_2xn_타일링 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        int[] dp = new int[N+2];
+        dp[1] = 1;
+        dp[2] = 2;
+        for (int i = 3; i <= N; i++) {
+            dp[i] = (dp[i-1] + dp[i-2])%10007;
+        }
+
+        System.out.println(dp[N]);
+    }
+}
