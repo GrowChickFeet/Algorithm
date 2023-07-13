ceec05a8288bcd2a68631e5ed81fc61d2dc642ff
김진아 <92143119+mikaniz@users.noreply.github.com>
2023-07-14 08:43:33 +0900

    BJ1758_알바생_강호 풀이 업로드
    팁이 깎이는 전체 합은 항상 같으므로 음수가 되는 팁이 적도록 팁을 내림차순으로 정렬한 뒤 계산하여 해결

---
+++ src/김진아/week14/BJ1758_알바생_강호.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+
+import java.util.Arrays;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int N = Integer.parseInt(reader.readLine());
+		int[] tips = new int[N];
+		for (int i = 0; i < N; i++) tips[i] = Integer.parseInt(reader.readLine());
+		
+		Arrays.sort(tips);
+		
+		long sum = 0;
+		for (int i = N - 1; i >= 0; i--) {
+			int tip = tips[i] - N + i + 1;
+			if (tip <= 0) break;
+			sum += tip;
+		}
+		
+		writer.write(Long.toString(sum));
+		
+		writer.close();
+		reader.close();
+	}
+
+}
