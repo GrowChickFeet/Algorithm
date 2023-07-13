f059062ac9b9f2ba56ca96b63881188bd953cece
김진아 <92143119+mikaniz@users.noreply.github.com>
2023-07-13 13:08:25 +0900

    BJ14719_ 빗물 풀이 업로드
    각 높이에 존재하는 기둥의 인덱스를 저장하고 저장된 인덱스의 차를 통해 빗물의 양을 계산하여 해결

---
+++ src/김진아/week14/BJ14719_ 빗물.java
@@ -0,0 +1,44 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+
+import java.util.ArrayList;
+
+public class Main {
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		String[] input = reader.readLine().split(" ");
+		int H = Integer.parseInt(input[0]);
+		int W = Integer.parseInt(input[1]);
+		
+		ArrayList<ArrayList<Integer>> blocks = new ArrayList<>();
+		for (int i = 0; i < H; i++) blocks.add(new ArrayList<>());
+		
+		input = reader.readLine().split(" ");
+		for (int i = 0; i < W; i++) {
+			int block = Integer.parseInt(input[i]);
+			for (int j = 0; j < block; j++) blocks.get(j).add(i);
+		}
+		
+		int water = 0;
+		for (int i = 0; i < H; i++) {
+			int count = blocks.get(i).size();
+			for (int j = 0; j < count - 1; j++) {
+				int now = blocks.get(i).get(j);
+				int next = blocks.get(i).get(j + 1);
+				if (next - now > 1) water += next - now - 1;
+			}
+		}
+		
+		writer.write(Integer.toString(water));
+		
+		writer.close();
+		reader.close();
+	}
+	
+}
