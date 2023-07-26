9dd35b25ad8c616693d3a81ea689c793b5246b1c
김진아 <92143119+mikaniz@users.noreply.github.com>
2023-07-27 00:08:10 +0900

    BJ2960_에라토스테네스의_체 풀이 업로드
    각 숫자에 대해 지워졌는지 여부를 저장하는 boolean 배열을 이용하여 해결

---
+++ src/김진아/week15/BJ2960_에라토스테네스의_체.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+
+public class Main {
+
+    static int N;
+    static int K;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String[] input = reader.readLine().split(" ");
+        N = Integer.parseInt(input[0]);
+        K = Integer.parseInt(input[1]);
+
+        writer.write(Integer.toString(deletion()));
+
+        writer.close();
+        reader.close();
+    }
+
+    static int deletion() {
+        boolean[] deleted = new boolean[N + 1];
+        for (int i = 2; i <= N; i++) {
+            if (deleted[i]) continue;
+            for (int j = i; j <= N; j += i) {
+                if (deleted[j]) continue;
+                deleted[j] = true;
+                if (--K == 0) return j;
+            }
+        }
+        return -1;
+    }
+
+}
