dd0dd56725bf834eef7dd74d433af5864ca65dda
김진아 <92143119+mikaniz@users.noreply.github.com>
2023-08-03 00:53:58 +0900

    BJ2491_수열 풀이 업로드
    수열을 순차 탐색하며 연속해서 커지는 수열의 길이, 연속해서 작아지는 수열의 길이를 갱신하여 해결

---
+++ src/김진아/week02/BJ2491_수열.java
@@ -0,0 +1,44 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(reader.readLine());
+
+        int[] nums = new int[N];
+        String[] input = reader.readLine().split(" ");
+        for (int i = 0; i < N; i++) nums[i] = Integer.parseInt(input[i]);
+
+        int max = 1;
+
+        int increase = 1;
+        int decrease = 1;
+        for (int i = 1; i < N; i++) {
+            if (nums[i] > nums[i - 1]) {
+                increase++;
+                decrease = 1;
+            } else if (nums[i] < nums[i - 1]) {
+                increase = 1;
+                decrease++;
+            } else {
+                increase++;
+                decrease++;
+            }
+
+            max = Math.max(Math.max(increase, decrease), max);
+        }
+
+        writer.write(Integer.toString(max));
+
+        writer.close();
+        reader.close();
+    }
+
+}
