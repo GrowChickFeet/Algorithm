1472c5cb3e1db014df6be44d7818e94b23b2423c
김진아 <92143119+mikaniz@users.noreply.github.com>
2023-08-20 20:50:02 +0900

    BJ15903_카드_합체_놀이 풀이 업로드
    우선순위 큐를 활용하여 해결

---
+++ src/김진아/week04/BJ15903_카드_합체_놀이.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+
+import java.util.PriorityQueue;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String[] input = reader.readLine().split(" ");
+        int n = Integer.parseInt(input[0]);
+        int m = Integer.parseInt(input[1]);
+
+        PriorityQueue<Long> queue = new PriorityQueue<>();
+        input = reader.readLine().split(" ");
+        for (int i = 0; i < n; i++) queue.offer(Long.parseLong(input[i]));
+
+        for (int i = 0; i < m; i++) {
+            long result = queue.poll() + queue.poll();
+            queue.offer(result);
+            queue.offer(result);
+        }
+
+        long sum = 0;
+        while (!queue.isEmpty()) sum += queue.poll();
+
+        writer.write(Long.toString(sum));
+
+        writer.close();
+        reader.close();
+    }
+
+}
