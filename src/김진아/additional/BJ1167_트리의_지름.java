768f17b65ec9f79358df0e00887c03f7133877f7
김진아 <92143119+mikaniz@users.noreply.github.com>
2023-09-04 22:14:18 +0900

    BJ1167_트리의_지름 풀이 업로드
    임의의 한 점에 대해 DFS를 수행하여 가장 먼 점을 찾은 뒤 해당 점에 대해 DFS를 수행하여 해결

---
+++ src/김진아/additional/BJ1167_트리의_지름.java
@@ -0,0 +1,61 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+
+import java.util.ArrayList;
+
+public class Main {
+
+    static ArrayList<ArrayList<int[]>> tree;
+
+    static int max;
+    static int point;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(reader.readLine());
+        tree = new ArrayList<>();
+        for (int i = 0; i < N; i++) tree.add(new ArrayList<>());
+
+        for (int i = 0; i < N; i++) {
+            String[] input = reader.readLine().split(" ");
+            int index = 0;
+            int node = Integer.parseInt(input[index++]) - 1;
+            while (!input[index].equals("-1")) tree.get(node).add(new int[] { Integer.parseInt(input[index++]) - 1, Integer.parseInt(input[index++]) });
+        }
+
+        max = 0;
+
+        boolean[] visited = new boolean[N];
+        visited[0] = true;
+        max(visited, 0, 0);
+
+        visited = new boolean[N];
+        visited[point] = true;
+        max(visited, point, 0);
+
+        writer.write(Integer.toString(max));
+
+        writer.close();
+        reader.close();
+    }
+
+    static void max(boolean[] visited, int now, int length) {
+        if (length > max) {
+            max = length;
+            point = now;
+        }
+
+        for (int[] next : tree.get(now)) {
+            if (visited[next[0]]) continue;
+            visited[next[0]] = true;
+            max(visited, next[0], length + next[1]);
+            visited[next[0]] = false;
+        }
+    }
+
+}
