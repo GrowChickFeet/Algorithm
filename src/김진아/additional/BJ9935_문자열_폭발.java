f9e8711417759bc5e223de2d5c5b8377d28ca147
김진아 <92143119+mikaniz@users.noreply.github.com>
2023-09-06 23:08:44 +0900

    BJ9935_문자열_폭발 풀이 업로드
    Stack을 활용하여 문자열의 앞 글자부터 Stack에 삽입하다가 폭발 문자열의 길이가 되는 순간 폭발 문자열의 존재 여부를 조사하여 해결

---
+++ src/김진아/additional/BJ9935_문자열_폭발.java
@@ -0,0 +1,52 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+
+import java.util.Stack;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String str = reader.readLine();
+        String bomb = reader.readLine();
+
+        int length = str.length();
+        int bombLength = bomb.length();
+
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < length; i++) {
+            stack.push(str.charAt(i));
+
+            int size = stack.size();
+            if (size < bombLength) continue;
+
+            boolean willBomb = true;
+            for (int j = 0; j < bombLength; j++) {
+                if (stack.get(size - 1 - j) != bomb.charAt(bombLength - 1 - j)) {
+                    willBomb = false;
+                    break;
+                }
+            }
+
+            if (willBomb) {
+                for (int j = 0; j < bombLength; j++) stack.pop();
+            }
+        }
+
+        if (stack.isEmpty()) writer.write("FRULA");
+        else {
+            StringBuilder builder = new StringBuilder();
+            for (char character : stack) builder.append(character);
+            writer.write(builder.toString());
+        }
+
+        writer.close();
+        reader.close();
+    }
+
+}
