b585c846f9a6bb10fcc5685da565689d120dc50f
김진아 <92143119+mikaniz@users.noreply.github.com>
2023-09-15 23:44:05 +0900

    BJ10815_숫자_카드 풀이 업로드
    Set을 활용하여 해결

---
+++ src/김진아/additional/BJ10815_숫자_카드.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+
+import java.util.HashSet;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder builder = new StringBuilder();
+
+        HashSet<Integer> numbers = new HashSet<>();
+
+        int N = Integer.parseInt(reader.readLine());
+        String[] input = reader.readLine().split(" ");
+        for (int i = 0; i < N; i++) numbers.add(Integer.parseInt(input[i]));
+
+        int M = Integer.parseInt(reader.readLine());
+        input = reader.readLine().split(" ");
+        for (int i = 0; i < M; i++) builder.append(numbers.contains(Integer.parseInt(input[i])) ? "1" : "0").append(" ");
+
+        writer.write(builder.toString());
+
+        writer.close();
+        reader.close();
+    }
+
+}
