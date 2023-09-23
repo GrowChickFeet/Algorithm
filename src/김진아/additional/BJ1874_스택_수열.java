028ae4ff0986ca0735cf41e5f0d3677ad06c760b
김진아 <92143119+mikaniz@users.noreply.github.com>
2023-09-23 22:16:45 +0900

    BJ1874_스택_수열 풀이 업로드
    스택을 활용하여 해결

---
+++ src/김진아/additional/BJ1874_스택_수열.java
@@ -0,0 +1,47 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+
+import java.util.Stack;
+
+public class Main {
+
+    static int n;
+    static int[] nums;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        n = Integer.parseInt(reader.readLine());
+        nums = new int[n];
+        for (int i = 0; i < n; i++) nums[i] = Integer.parseInt(reader.readLine());
+
+        writer.write(makeNums());
+
+        writer.close();
+        reader.close();
+    }
+
+    static String makeNums() {
+        StringBuilder builder = new StringBuilder();
+
+        Stack<Integer> stack = new Stack<>();
+        int num = 1;
+
+        for (int i = 0; i < n; i++) {
+            while (num <= nums[i]) {
+                stack.push(num++);
+                builder.append("+\n");
+            }
+
+            if (stack.pop() != nums[i]) return "NO";
+            builder.append("-\n");
+        }
+
+        return builder.toString();
+    }
+
+}
