6b5b2262fb5b57c0e6bf194b8c5abd35ee80c84b
김진아 <92143119+mikaniz@users.noreply.github.com>
2023-09-26 22:23:09 +0900

    BJ11279_최대_힙 풀이 업로드
    우선순위 큐를 활용하여 해결

---
+++ src/김진아/additional/BJ11279_최대_힙.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+
+import java.util.PriorityQueue;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder builder = new StringBuilder();
+
+        PriorityQueue<Integer> queue = new PriorityQueue<>((o1, o2) -> o2 - o1);
+
+        int N = Integer.parseInt(reader.readLine());
+        for (int i = 0; i < N; i++) {
+            int command = Integer.parseInt(reader.readLine());
+            if (command != 0) queue.offer(command);
+            else {
+                if (queue.isEmpty()) builder.append("0\n");
+                else builder.append(queue.poll()).append("\n");
+            }
+        }
+
+        writer.write(builder.toString());
+
+        writer.close();
+        reader.close();
+    }
+
+}
