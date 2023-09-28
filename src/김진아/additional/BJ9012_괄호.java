c83c2444d178df382fbe04532e64e1ddb23d1bb8
김진아 <92143119+mikaniz@users.noreply.github.com>
2023-09-28 23:42:02 +0900

    BJ9012_괄호 풀이 업로드
    여는 괄호의 개수를 세고 닫는 괄호가 나올 때마다 여는 괄호의 개수를 확인하여 해결

---
+++ src/김진아/additional/BJ9012_괄호.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder builder = new StringBuilder();
+
+        int T = Integer.parseInt(reader.readLine());
+        for (int i = 0; i < T; i++) {
+            if (isValid(reader.readLine())) builder.append("YES\n");
+            else builder.append("NO\n");
+        }
+
+        writer.write(builder.toString());
+
+        writer.close();
+        reader.close();
+    }
+
+    static boolean isValid(String str) {
+        int length = str.length();
+        int count = 0;
+
+        for (int i = 0; i < length; i++) {
+            if (str.charAt(i) == '(') count++;
+            else {
+                if (--count < 0) return false;
+            }
+        }
+
+        return count == 0;
+    }
+
+}
