ab791f41f28aca9ac2c70277b34c29226a1a0877
김진아 <92143119+mikaniz@users.noreply.github.com>
2023-09-30 23:31:33 +0900

    BJ11004_K번째_수 풀이 업로드

---
+++ src/김진아/additional/BJ11004_K번째_수.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String[] input = reader.readLine().split(" ");
+        int N = Integer.parseInt(input[0]);
+        int K = Integer.parseInt(input[1]);
+
+        int[] nums = new int[N];
+        input = reader.readLine().split(" ");
+        for (int i = 0; i < N; i++) nums[i] = Integer.parseInt(input[i]);
+
+        Arrays.sort(nums);
+
+        writer.write(Integer.toString(nums[K - 1]));
+
+        writer.close();
+        reader.close();
+    }
+
+}
