650711743386e547d2f2cadf6c416c3a132e63ed
김진아 <92143119+mikaniz@users.noreply.github.com>
2023-10-03 15:59:24 +0900

    BJ1644_소수의_연속합 풀이 업로드
    투포인터를 활용하여 해결

---
+++ src/김진아/week18/BJ1644_소수의_연속합.java
@@ -0,0 +1,47 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+
+import java.util.ArrayList;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(reader.readLine());
+
+        ArrayList<Integer> primes = new ArrayList<>();
+        for (int i = 2; i <= N; i++) {
+            if (isPrime(i)) primes.add(i);
+        }
+
+        int length = primes.size();
+        int count = 0;
+        int sum = 0;
+        int end = 0;
+
+        for (int start = 0; start < length; start++) {
+            if (sum >= N || end == length) sum -= primes.get(start - 1);
+            while (end < length && sum < N) sum += primes.get(end++);
+            if (sum == N) count++;
+        }
+
+        writer.write(Integer.toString(count));
+
+        writer.close();
+        reader.close();
+    }
+
+    static boolean isPrime(int N) {
+        int mid = (int) Math.sqrt(N);
+        for (int i = 2; i <= mid; i++) {
+            if (N % i == 0) return false;
+        }
+        return true;
+    }
+
+}
