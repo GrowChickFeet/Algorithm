5d420882474645aa4c728b7be31f58523cef173c
김진아 <92143119+mikaniz@users.noreply.github.com>
2023-10-06 08:06:12 +0900

    BJ1302_베스트셀러 풀이 업로드
    Map을 활용하여 해결

---
+++ src/김진아/additional/BJ1302_베스트셀러.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+
+import java.util.HashMap;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        HashMap<String, Integer> books = new HashMap<>();
+        int N = Integer.parseInt(reader.readLine());
+        for (int i = 0; i < N; i++) {
+            String book = reader.readLine();
+            books.put(book, books.getOrDefault(book, 0) + 1);
+        }
+
+        String bestseller = "";
+        int bestsellerCount = 0;
+        for (String book : books.keySet()) {
+            int count = books.get(book);
+            if (count > bestsellerCount || (count == bestsellerCount && book.compareTo(bestseller) < 0)) {
+                bestseller = book;
+                bestsellerCount = count;
+            }
+        }
+
+        writer.write(bestseller);
+
+        writer.close();
+        reader.close();
+    }
+
+}
